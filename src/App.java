c45a89a76de5a819cc2bcb6db47f3b88317c8a9e
EDWARDCHUNG96C8\edward <leonardo071123@gmal.com>
2025-04-22 15:50:19 +0800

    update: test for merge

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! I am Edward No. 1!");
+
+        System.out.println("Hello, World! I am Edward No. 17!");
     }
 }
