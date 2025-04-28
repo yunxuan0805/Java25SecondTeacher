f927539cc68f9ae94a50aeb43e1587f946592e29
Edward <leonardo071213@gmail.com>
2025-04-28 10:08:38 +0800

    update: 增加補充 & 修正範例

--- src/LogicalOperation.java
+++ src/LogicalOperation.java
@@ -57,7 +57,7 @@ public class LogicalOperation {
         /*
          *  == : 等於
          */
-        // int a = 5;
+        // int a = 12;
         // int b = 12;
 
         // boolean isEqual = (a == b);
@@ -97,6 +97,8 @@ public class LogicalOperation {
         /*
          *  >= : 大於或等於
          */
+        // int a = 12;
+        // int b = 12;
         //  boolean greaterThenOrEqual = (a >= b);
         //  System.out.println(greaterThenOrEqual);
  
@@ -110,18 +112,24 @@ public class LogicalOperation {
         /*
          *  && : 並且
          */
-        // boolean and = (a < b) && (b > a);
+        // int a = 3;
+        // int b = 5;
+        // boolean and = (a == 2) && (b == 5);
         // System.out.println(and);
 
         /*
          *  || : 或者
          */
-        // boolean or = (a < b) || (b < a);
+        // int a = 3;
+        // int b = 5;
+        // boolean or = (a == 1) || (b == 4);
         // System.out.println(or);
 
         /*
          *  !  : 反轉
          */
+        // int a = 3;
+        // int b = 4;
         // boolean not = !(a < b);
         // System.out.println(not);
 
@@ -129,6 +137,7 @@ public class LogicalOperation {
          *  ++ : 自增1
          */
         // int a = 5;
+        // // a = a + 1;
         // a++;
         // System.out.println(a);
 
@@ -136,6 +145,7 @@ public class LogicalOperation {
          *  -- : 自減1
          */
         // int a = 5;
+        // // a = a - 1;
         // a--;
         // System.out.println(a);
 
@@ -144,7 +154,17 @@ public class LogicalOperation {
          */
         // int a = 5;
         // a += 2;
+        // // a = a + 2;
         // System.out.println(a);
+        
+
+        /*
+         * 250428補充: 字串串接別的文字
+         */
+        // String test1 = "I am";
+        // test1 = test1 + " a teacher";
+        // System.out.println(test1);
+
 
         /*
          *  -= : 減法後再賦值
